4458d31436b6986755ca07c728ff1a17626e0e18
iyeahh <yboravender@gmail.com>
2023-10-03 16:22:32 +0900

    First Commit

---
+++ ThinkingAbout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ThinkingAbout
+//
+//  Created by Bora Yang on 2023/10/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
