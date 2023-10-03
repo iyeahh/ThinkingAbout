92ebc153f17aec98c8e34097397d7015a8caa779
iyeahh <yboravender@gmail.com>
2023-10-03 17:05:44 +0900

    feat: List 화면 UI 구현 및 코드 연결

---
+++ ThinkingAbout/ListCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ListCollectionViewCell.swift
+//  ThinkingAbout
+//
+//  Created by Bora Yang on 2023/10/03.
+//
+
+import UIKit
+
+class ListCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var categoryImageView: UIImageView!
+    @IBOutlet weak var categoryLabel: UILabel!
+    @IBOutlet weak var numberOfTaskLabel: UILabel!
+}
---
+++ ThinkingAbout/ListViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ListViewController.swift
+//  ThinkingAbout
+//
+//  Created by Bora Yang on 2023/10/03.
+//
+
+import UIKit
+
+final class ListViewController: UIViewController {
+
+    @IBOutlet weak var mainLabel: UILabel!
+    @IBOutlet weak var collectionView: UICollectionView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func addButtonTapped(_ sender: UIButton) {
+    }
+}
+
