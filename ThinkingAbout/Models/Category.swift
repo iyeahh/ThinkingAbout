6cdf59dfc09d99dc50b0552a8d0e355d241a8482
iyeahh <yboravender@gmail.com>
2023-10-04 13:41:20 +0900

    refactor: coreData Entity 수정

---
+++ ThinkingAbout/Models/Category.swift
@@ -0,0 +1,14 @@
+//
+//  Category.swift
+//  ThinkingAbout
+//
+//  Created by Bora Yang on 2023/10/04.
+//
+
+import UIKit
+
+struct Category {
+    var type: String?
+    var color: UIColor?
+    var image: UIImage?
+}
